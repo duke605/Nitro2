0ca4b30f2b04e679373b1ebc962187330e5e2b1e
Duke605 <cmdenslow14@hotmail.com>
2017-09-25 23:07:25 -0500

    Incremenented version number

--- cogs/meta.py
+++ cogs/meta.py
@@ -8,7 +8,7 @@ class Meta:
 
     def __init__(self, bot):
         self.bot = bot
-        self.version = '0.2.4'
+        self.version = '0.2.5'
         self.counter = Counter()
 
     async def on_command_completion(self, command, ctx):
